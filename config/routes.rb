60222a25ecb2a3b19b3d435f3d70fb406da84d8b
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-10-01 00:51:41 +0100

    got the get to work, maybe getting the post to work?

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'comments#index'
+
+  post 'comments#create', defaults: {format: :json}
+  get 'comments#index', defaults: {format: :json}
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
